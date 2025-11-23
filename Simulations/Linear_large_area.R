d9f5c642ecf8d2b7ebc6eb8ebad3f0a9f0d83ee9
Rutten-Sara <sara.rutten@uhasselt.be>
2025-11-23 03:54:40 +0100

    Update Linear_large_area.R

--- Simulations/Linear_large_area.R
+++ Simulations/Linear_large_area.R
@@ -9,22 +9,20 @@ library(dlnm)
 library(tsModel); library(gnm)
 library(R.utils);library(mixmeta); library(splines)
 
-setwd("G:/My Drive/Onderzoek/DLNM/Spatial varying DLNM/Code/Covariate varying")
-
-source('functions/DLNM_Laplace_covariate.R')
-source('functions/DLNM_Laplace_covariate_NB.R')
-source('functions/DLNM_Laplace_covariate_poisson.R')
-source('functions/predRR_covariate.R')
-source('functions/help_functions.R')
-source('functions/BAM_predict.R')
-source('functions/interpret_coefficients.R')
+source('../functions/DLNM_Laplace_covariate.R')
+source('../functions/DLNM_Laplace_covariate_NB.R')
+source('../functions/DLNM_Laplace_covariate_poisson.R')
+source('../functions/predRR_covariate.R')
+source('../functions/help_functions.R')
+source('../functions/BAM_predict.R')
+source('../functions/interpret_coefficients.R')
 
 
 
 # Load data
 
-load("data/daily_data.RData")
-shapefile_bcn <- read_sf("data/shapefile_bcn.shp")
+load("../data/daily_data.RData")
+shapefile_bcn <- read_sf("../data/shapefile_bcn.shp")
 data$year <- year(data$date)
 
 
@@ -902,5 +900,6 @@ ggplot(df_exposure_long, aes(x = x, y = exp(RR), group = row, color = row)) +
   theme_minimal() +
   ggtitle("Linear")
 dev.off()
+
 
 
