9217798251057b191899bb82ab6a51c0025d8f17
Rutten-Sara <sara.rutten@uhasselt.be>
2026-01-16 16:30:28 +0100

    Update Linear_small_area.R

--- Simulations/Linear_small_area.R
+++ Simulations/Linear_small_area.R
@@ -314,7 +314,7 @@ for (i in 1:nsim){
     interaction_matrix <- do.call(cbind, interaction_list)
     
     mtime <- proc.time()
-    mod_cts <- gnm(y_all ~ z_lin + crossbasis_unpen + interaction_matrix, 
+    mod_cts <- gnm(y_all ~ crossbasis_unpen + interaction_matrix, 
                    eliminate=stratum, data=data, family=poisson)
     time_CTS =  (proc.time()-mtime)[3]
     
@@ -853,5 +853,6 @@ which_fails/tot_sim
 time/tot_sim
 DIC_percentage/tot_sim
 AIC_percentage/tot_sim
+
 
 
